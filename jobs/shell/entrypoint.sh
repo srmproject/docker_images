ac576878788628d27ce9d95e07ebf7863176c72f
choisungwook <kgg1959@naver.com>
2021-12-19 20:27:14 +0900

    실행로그 추가

--- jobs/shell/entrypoint.sh
+++ jobs/shell/entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 
 git clone ${GIT_URL} -b ${GIT_BRANCH} workspace
 cd workspace
+echo "git clone is done"
 
 # change dir
 dir_name=$(dirname "${FILEPATH}")
@@ -12,4 +13,4 @@ file_name=$(dirname "${FILEPATH}")
 cd ${dir_name}
 sh ${file_name}
 
-echo "done"
+echo "execute file is done"
