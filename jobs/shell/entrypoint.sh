fdff1042ccb6f5359fa8592bddfb7c631528ecf7
choisungwook <kgg1959@naver.com>
2021-12-19 20:30:28 +0900

    디버깅 로그 추가

--- jobs/shell/entrypoint.sh
+++ jobs/shell/entrypoint.sh
@@ -3,13 +3,14 @@
 set -e
 
 git clone ${GIT_URL} -b ${GIT_BRANCH} workspace
-cd workspace
 echo "git clone is done"
 
 # change dir
 dir_name=$(dirname "${FILEPATH}")
 file_name=$(dirname "${FILEPATH}")
 
+cd workspace
+ls
 cd ${dir_name}
 sh ${file_name}
 
