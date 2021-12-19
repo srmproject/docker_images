32e551117ccc4a1270dfa12beab0c540dd1c14b5
choisungwook <kgg1959@naver.com>
2021-12-19 20:33:50 +0900

    디버깅 로그 추가

--- jobs/shell/entrypoint.sh
+++ jobs/shell/entrypoint.sh
@@ -10,7 +10,15 @@ dir_name=$(dirname "${FILEPATH}")
 file_name=$(dirname "${FILEPATH}")
 
 cd workspace
-ls
+echo "current: location"
+pwd
+
+echo "ls"
+ls -l
+
+echo "dirname: ${dir_name}"
+echo "file_name: ${file_name}"
+
 cd ${dir_name}
 sh ${file_name}
 
