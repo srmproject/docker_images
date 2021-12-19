39ec289934f9cea07b014647873e4b3177639ffd
choisungwook <kgg1959@naver.com>
2021-12-19 20:02:35 +0900

    쉘스크립트 실행 코드 ㅊ가

--- jobs/shell/entrypoint.sh
+++ jobs/shell/entrypoint.sh
@@ -5,4 +5,11 @@ set -e
 git clone ${GIT_URL} workspace
 cd workspace
 
+# change dir
+dir_name=$(dirname "${FILEPATH}")
+file_name=$(dirname "${FILEPATH}")
+
+cd ${dir_name}
+sh ${file_name}
+
 echo "done"
