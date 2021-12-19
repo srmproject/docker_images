0fea730de71710d5a12d7bd94eceb7932a5438df
choisungwook <kgg1959@naver.com>
2021-12-19 20:42:17 +0900

    실행스크립트 수정

--- jobs/shell/entrypoint.sh
+++ jobs/shell/entrypoint.sh
@@ -6,21 +6,9 @@ git_workspace="workspace"
 git clone ${GIT_URL} -b ${GIT_BRANCH} ${git_workspace}
 echo "git clone is done"
 
-# change dir
 dir_name=$(dirname "${FILEPATH}")
 file_name=$(dirname "${FILEPATH}")
 
-cd $(${git_workspace} "$0")
-echo "current: location"
-pwd
-
-echo "ls"
-ls -l
-
-echo "dirname: ${dir_name}"
-echo "file_name: ${file_name}"
-
-cd ${dir_name}
-sh ${file_name}
+cd ${git_workspace}/${dirname} && sh ${file_name}
 
 echo "execute file is done"
